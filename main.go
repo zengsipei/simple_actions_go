0f952db95476f51b04e41aee23fd8bc891d0c677
曾斯培 <zengsp.mail@qq.com>
2024-01-05 14:35:37 +0800

    测试

--- main.go
+++ main.go
@@ -10,7 +10,67 @@ import (
 	"simple_actions/utils"
 	"strconv"
 	"strings"
+	"io"
+	"net/http"
 )
+
+func d(values ...interface{}) {
+	for _, v := range values {
+		fmt.Printf("%T - %v\n", v, v)
+	}
+}
+
+func de(values ...interface{}) {
+	d(values...)
+	os.Exit(0)
+}
+
+// Request 发送 HTTP 请求并返回响应体的内容
+func Request(method, remoteUrl string, body io.Reader, headers map[string]string) []byte {
+	d(remoteUrl)
+	tr := &http.Transport{
+		//TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	// 创建一个 HTTP 客户端
+	client := &http.Client{Transport: tr}
+
+	// 创建一个 HTTP 请求
+	req, err := http.NewRequest(method, remoteUrl, body)
+	if err != nil {
+		fmt.Println("Error occurred:", err)
+		return nil
+	}
+
+	// 设置请求的 header
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+
+	// 发送请求并获取响应
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error occurred:", err)
+		return nil
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		fmt.Printf("Request failed with status code %d\n", resp.StatusCode)
+		return nil
+	}
+
+	content, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return nil
+	}
+	if len(content) == 0 {
+		fmt.Println("Content is empty")
+		return nil
+	}
+
+	return content
+}
 
 func main() {
 	viper.SetConfigName("base")
@@ -26,9 +86,7 @@ func main() {
 	proxies := proxy["proxies"].([]interface{})
 	now := carbon.Now()
 	d(now)
-	flag := true
-	for flag {
-		flag = false
+	for {
 		// v2rayshare.com
 		filename := now.Format("20060102") + ".yaml"
 		urlArr := []string{
@@ -65,11 +123,9 @@ func main() {
 
 		v2rayProxies := v2rayViper.Get("proxies").([]interface{})
 		proxies = append(proxies, v2rayProxies...)
+		break
 	}
-
-	flag = true
-	for flag {
-		flag = false
+	for {
 		isExpire := true
 		// MrdoorVpn
 		file, err := os.ReadFile("mrdoor.json")
@@ -174,6 +230,7 @@ func main() {
 			if err != nil {
 			}
 		}
+		break
 	}
 
 	viper.Set("proxies", proxies)
